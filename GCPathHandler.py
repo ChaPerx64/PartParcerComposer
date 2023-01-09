e044c48228d53816b66023eb9cd34b995c40a3b4
ChaPerx64 <chayan.par@gmail.com>
2023-01-09 19:34:51 +0300

    - main window designed and created - created lib 'GCPathHandler' for handling path strings

---
+++ GCPathHandler.py
@@ -0,0 +1,43 @@
+import os
+
+class CDPath:
+    def __init__(self, path: os.PathLike):
+        self.path = str(path)
+
+
+def is_gc_directory(path: str):
+    pathway_list = path.split('/')
+    if pathway_list[len(pathway_list) - 1] == 'GrabCAD':
+        return True
+    else:
+        return False
+
+def is_in_gc(path: str):
+    if 'GrabCAD' in path:
+        return True
+    else:
+        return False
+
+
+def is_project_folder(path: str):
+    pathway_list = path.split('/')
+    if pathway_list[len(pathway_list) - 2] == 'GrabCAD':
+        return True
+    else:
+        return False
+
+
+def get_project_pathway(path: str):
+    if is_in_gc(path) and not is_gc_directory(path):
+        pathway_list = path.split('/')
+        while pathway_list[len(pathway_list)-2] != 'GrabCAD':
+            # print(pathway_list[len(pathway_list)-2])
+            pathway_list.pop(len(pathway_list)-1)
+            # pass
+        path_out = '/'.join(pathway_list)
+        return path_out
+    else:
+        raise NotADirectoryError('Этот путь не ведет в ПОДпапку GrabCAD!')
+
+# pth = 'D:/Workfolder/Workfolder_CAD/Photomechanics/GrabCAD/Familia/PMA.RCK.00.000/CAD/PMA.RCK.00.000.SLDASM'
+# print(get_project_pathway(pth))
--- main.py
+++ main.py
@@ -1,35 +1,86 @@
 import PySimpleGUI as sg
 import os.path
 import PartParser
-# sg.Window(title="Hello World", layout=[[]])
-H1_FONT = ("Arial", 20)
+import sys
+import GCPathHandler as ph
+from configparser import ConfigParser
 
-# create layout
+# Load or create the configuration file
+CONFIG = {}
+try:
+    configur = ConfigParser()
+    configur.read("config.ini")
+    CONFIG.update({"GRABCAD_PATH": configur.get("paths", "grabcad_path")})
+    # GRABCAD_PATH = )
+except:
+    configur = None
+
+
+# Constants for style)
+H1_FONT = ("Segoe UI", 22)
+H2_FONT = ("Segoe UI", 18)
+DEFAULT_FONT = ('Segoe UI', 11)
+SMALL_FONT = ("Segoe UI", 8)
+SPEC_TYPES = ("Плоская спецификация",)
+
+# Create layout
 layout = [
-    [sg.Text("Welcome to PartParser!", font=H1_FONT)],
-    [sg.Text("Выбери Excel-файл")],
-    [[sg.In(enable_events=True)], [sg.FileBrowse()]],
-    [sg.Button("Check")]
+    [sg.Push(), sg.Text("Welcome to PartParser!", font=H1_FONT), sg.Push()],
+    [sg.HSeparator()],
+    [sg.Text("Спецификация для парсинга", font=H2_FONT)],
+    [sg.Text("Выбери Excel-файл"), sg.In(enable_events=True, key='-XLSX-'), sg.FileBrowse(key='XBrowse')],
+    [sg.Text("Выбери тип спецификации"), sg.Listbox(SPEC_TYPES,
+                                             no_scrollbar=True,
+                                             size=(60, 2),
+                                             font=SMALL_FONT,
+                                             select_mode="LISTBOX_SELECT_MODE_SINGLE",
+                                             )],
+    [sg.HSeparator()],
+    [sg.Text("Область поиска", font=H2_FONT)],
+    [sg.Text("Папка поиска"), sg.In(enable_events=True,
+                                    key="-GCFolder-",
+                                    ), sg.FolderBrowse("Изменить", key='FBrowse')],
+    [sg.Checkbox("Искать в 'Sorters'"),
+     sg.Checkbox("Искать в 'Design Standarts'"),
+     sg.Checkbox("Искать по всему 'GrabCAD'")],
+    [sg.HSeparator()],
+    [sg.Text("Параметры поиска", font=H2_FONT)],
+    [sg.Checkbox("Пробовать поиск групповых чертежей", default=True), sg.Checkbox("Выводить log-файл", default=True)],
+    [sg.HSeparator()],
+    [sg.Push(), sg.Button("Искать"), sg.Push()]
 ]
 
-# sg.Graph
-# sg.
-
+# create window
 window = sg.Window(
     "PartParserComposer",
     layout,
-    margins=(100, 300),
-    resizable=True,
-    element_justification="center"
-    # finalize=True
-    # no_titlebar=True
+    size=(700, 500),
+    element_justification="left",
+    font=DEFAULT_FONT,
+    finalize=True
 )
 
+
+def create_config(config):
+    configur = ConfigParser()
+    gc_path = sg.popup_get_folder('Config-файл не найден. Необходима настройка. Укажи корневую папку GrabCAD.')
+    configur.add_section("paths")
+    configur.set("paths", "grabcad_path", gc_path)
+    with open('config.ini', 'w') as conf:
+        configur.write(conf)
+
+
 if __name__ == '__main__':
-    # sg.Window("PartParserComposer", layout, margins=(100, 300), no_titlebar=True).read()
-    print('foo')
+    if configur is None:
+        create_config(CONFIG)
+    # window['-GCFolder-'].update(CONFIG['GRABCAD_PATH'])
     while True:
         event, values = window.read()
         if event in ("Exit", sg.WIN_CLOSED):
             break
+        # window['-GCFolder-'].update(CONFIG['GRABCAD_PATH'])
+        # if event is :
+        # window[]
     window.close()
+    # path = GCPathHandler.CDPath(CONFIG['GRABCAD_PATH'])
+    # print(GCPathHandler.get_project_pathway(CONFIG['GRABCAD_PATH']))
