970e0265aac05b0e372fe58bc2b499e719c4fe18
ChaPerx64 <chayan.par@gmail.com>
2023-03-17 12:08:47 +0100

    - renamed local variables according to PEP - made get_window method more universal (it returns window with the layout needed based on the winname used when called)

--- winconstructor.py
+++ winconstructor.py
@@ -1,60 +1,99 @@
 import PySimpleGUI as sg
 from blankshandler import BlanksHandler
 
-H1_FONT = ("Segoe UI", 22)
-H2_FONT = ("Segoe UI", 12)
-DEFAULT_FONT = ('Segoe UI', 10)
-SMALL_FONT = ("Segoe UI", 8)
-SPECTIP = 'Спецификация должна быть экспортирована из Солида "смещенной" с "подробной нумерацией"'
-BLANKTIP = 'Бланк должен соответствовать правилам. См. файл "INFO"'
+_H1_FONT = ("Segoe UI", 22)
+_H2_FONT = ("Segoe UI", 12)
+_DEFAULT_FONT = ('Segoe UI', 10)
+_SMALL_FONT = ("Segoe UI", 8)
+_PAGE_1 = True
+_SPECTIP = 'Спецификация должна быть экспортирована из Солида "смещенной" с "подробной нумерацией"'
+_BLANKTIP = 'Бланк должен соответствовать правилам. См. файл "INFO"'
 
 
 class WinConstructor:
     def __init__(self, bhandler: BlanksHandler):
+        self._config_dict = dict()
         self.bhandler = bhandler
-        pass
 
-    def b_cb_layout(self):
+    def _b_cb_layout(self):
         out_list = list()
         i = 0
         for entry in self.bhandler.get_blanks_names():
-            out_list.append([sg.Checkbox(entry, key=entry, font=SMALL_FONT)])
+            out_list.append([sg.Checkbox(entry, key=entry, font=_SMALL_FONT)])
             i += 1
         return out_list
 
+    def _params_list_layout(self):
+        in_list = [
+            # Label  param.keyword    event_key
+            ['Автор', '<AUTHOR>', '-CONFIG_AUTHOR-'],
+            ['Путь к шаблонам', 'blanks_path', '-CONFIG_BLANKS_PATH-'],
+            ['Сортировка 1', 'SPEC_SORT_1', '-CONFIG_SPEC_SORT_1-'],
+            ['Сортировка 2', 'SPEC_SORT_2', '-CONFIG_SPEC_SORT_2-'],
+        ]
+        out_list = list()
+        for label, mask, keyword in in_list:
+            out_list.append([sg.Text(label), sg.In(self._config_dict.get(mask), key=keyword)])
+        return out_list
+
     def get_main_layout(self):
-        main_layout = [
+        return [
             [sg.VPush()],
-            [sg.Push(), sg.Text("Welcome to PartParser!", font=H1_FONT), sg.Push()],
-            [sg.Push(), sg.Text("Формирование заказа", font=H2_FONT), sg.Push()],
+            [sg.Push(), sg.Text("Welcome to PartParser!", font=_H1_FONT), sg.Push()],
+            [sg.Push(), sg.Text("Формирование заказа", font=_H2_FONT), sg.Push()],
             [sg.HSeparator()],
-            [sg.Text('Введи наименование изделия', font=H2_FONT)],
-            [sg.In(key='-PROJECTNAME-', expand_x=True, tooltip=SPECTIP)],
+            [sg.Text('Введи наименование изделия', font=_H2_FONT)],
+            [sg.In(key='-PROJECTNAME-', expand_x=True, tooltip=_SPECTIP)],
             [sg.HSeparator()],
-            [sg.Text("Выбери эксель-файл спецификации", font=H2_FONT)],
-            [sg.In(key='-XLSX-', expand_x=True, tooltip=SPECTIP), sg.FileBrowse(key='-XBrowse-')],
+            [sg.Text("Выбери эксель-файл спецификации", font=_H2_FONT)],
+            [sg.In(key='-XLSX-', expand_x=True, tooltip=_SPECTIP), sg.FileBrowse(key='-XBrowse-')],
             [sg.HSeparator()],
-            [sg.Text("Выбери бланки для заполнения из стандартных", font=H2_FONT)],
-            self.b_cb_layout(),
-            [sg.Text("Можешь добавить кастомный бланк для заполнения", font=H2_FONT)],
-            [sg.In(key='-SPECSAVE-', expand_x=True, tooltip=BLANKTIP), sg.FileBrowse(key='-SBrowse-')],
+            [sg.Text("Выбери бланки для заполнения из стандартных", font=_H2_FONT)],
+            self._b_cb_layout(),
+            [sg.Checkbox("Кастомный бланк:", font=_SMALL_FONT, key='-CUSTOMBLANK_CB-'),
+             sg.In(key='-SPECSAVE-', expand_x=True, tooltip=_BLANKTIP, font=_SMALL_FONT),
+             sg.FileBrowse(key='-SBrowse-')],
             [sg.HSeparator()],
-            [sg.Text("Сохранение результата", font=H2_FONT)],
+            [sg.Text('Данные для заполнения', font=_H2_FONT)],
+            [sg.Text('Путь к документации', font=_SMALL_FONT),
+             sg.In(key='-SPECSAVE-', expand_x=True, tooltip=_BLANKTIP, font=_SMALL_FONT),
+             sg.FileBrowse()
+             ],
+            [sg.HSeparator()],
+            [sg.Text("Сохранение результата", font=_H2_FONT)],
             [sg.Checkbox('Сохранить в подпапке "/дата-время"', key='-DEFSAVE-', enable_events=True, default=True)],
             [sg.Text("Или выбери другое место")],
-            [sg.In(enable_events=True, key="-SaveFolder-", expand_x=True, disabled=True), sg.FolderBrowse(key='-FBrowse-', disabled=True)],
+            [sg.In(enable_events=True, key="-SaveFolder-", expand_x=True, disabled=True),
+             sg.FolderBrowse(key='-FBrowse-', disabled=True)],
             [sg.Push(), sg.Button("Парсить"), sg.Push(), sg.Button('config')],
             [sg.VPush()]
         ]
-        return main_layout
 
-    def get_main_window(self):
+    def get_config_layout(self):
+        return [
+            [sg.VPush()],
+            [sg.Push(), sg.Text('Настройки PartParser', font=_H1_FONT), sg.Push()],
+            [sg.VPush()],
+            self._params_list_layout(),
+            [sg.VPush()],
+            [sg.Push(), sg.Button('Сохранить', key='-SAVECONFIG-'), sg.Button('На главную', key='-GOHOME-')]
+        ]
+
+    def get_window(self, winname: str, config_dict: dict):
+        self._config_dict = config_dict
+        if winname == 'main':
+            layout = self.get_main_layout()
+            el_just = 'left'
+        elif winname == 'config':
+            layout = self.get_config_layout()
+            el_just = 'right'
         return sg.Window(
             "PartParserComposer",
-            self.get_main_layout(),
-            size=(600, 800),
+            layout,
+            # size=(600, 800),
+            resizable=True,
             margins=(30, 30),
-            element_justification="left",
-            font=DEFAULT_FONT,
+            element_justification=el_just,
+            font=_DEFAULT_FONT,
             finalize=True
         )
